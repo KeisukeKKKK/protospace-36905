1d8195255baa004699c75ca46e47278d035affdf
KeisukeKKKK <90385361+KeisukeKKKK@users.noreply.github.com>
2021-10-07 01:00:29 +0900

    ユーザー管理機能の実装

--- db/migrate/20210928092624_create_twees.rb
+++
@@ -1,8 +0,0 @@
-class CreateTwees < ActiveRecord::Migration[6.0]
-  def change
-    create_table :twees do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_092624) do
+ActiveRecord::Schema.define(version: 2021_10_05_062734) do
 
-  create_table "twees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.string "profile"
+    t.string "occupation"
+    t.string "position"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
